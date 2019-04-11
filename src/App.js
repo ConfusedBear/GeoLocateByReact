6d7e9ca49cbf02fa0761b6a54605ffeb2409da27
root <root@163.com>
2019-04-11 22:48:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+export default class App extends Component {
+
+constructor(props) {
+  super(props)
+  this.state = {
+    msg : "wow",
+    jingdu : "",
+    weidu : ""
   }
+
 }
 
-export default App;
+
+success = (pos) => {
+  var lng = pos.coords.longitude
+  var lat = pos.coords.latitude
+
+  this.setState ({
+    msg : "lng: " + lng + "lat: " + lat,
+    jingdu : lng,
+    weidu : lat,
+  })
+}
+
+error = (err) => {
+  this.setState({
+    msg : "Error: " + err
+  })
+}
+
+
+requestLocation = () => {
+  var options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0
+  }
+
+  // eslint-disable-next-line no-undef
+  navigator.geolocation.getCurrentPosition(this.success, this.error, options)
+}
+
+
+  render() {
+    this.requestLocation()
+
+    return (
+      <div>
+      <h1>test</h1>
+        <b>{this.state.msg}</b>
+      </div>
+    )
+  }
+}
