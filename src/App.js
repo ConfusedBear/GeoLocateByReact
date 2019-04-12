4de2cd9b3101f62aae2870ab3fbd4338967f646a
ConfusedBear <317119190@qq.com>
2019-04-12 18:06:18 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import fetchJsonp from 'fetch-jsonp'
+import { Button } from 'antd'
+import './App.css'
 
 
 export default class App extends Component {
@@ -8,24 +10,24 @@ export default class App extends Component {
     super(props)
     this.state = {
       msg: "wow",
-      GPSlng: "120",
-      GPSlat: "30",
-      Baidulng: "120",
-      Baidulat: "30",
+      GPSlng: "0",
+      GPSlat: "0",
+      Baidulng: "0",
+      Baidulat: "0",
       address: "读取中"
     }
-    
-    
+
   }
 
-  GPStoBaidu = () => {
+
+  GPStoBaidutoAddress = () => {
     let url = "http://api.map.baidu.com/geoconv/v1/?coords=" + this.state.GPSlng + "," + this.state.GPSlat + "&from=1&to=5&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
-    console.log("url:"+url)
+    console.log("url:" + url)
     fetchJsonp(url, {
       method: "GET",
       mode: 'cors',
       headers: {
-        Accept : 'application/json'
+        Accept: 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
         // 'Access-Control-Allow-Origin': '*'
       },
@@ -35,23 +37,106 @@ export default class App extends Component {
     }).then(data => {
       let temp = data["result"]
       temp = temp[0]
-      // this.state.Baidulng = temp["x"]
-      // this.state.Baidulat = temp["y"]
+      this.state.Baidulng = temp["x"]
+      this.state.Baidulat = temp["y"]
       this.setState({
-        Baidulng: temp["x"],
-        Baidulat: temp["y"],
+
       })
+      url = "http://api.map.baidu.com/geocoder/v2/?location=" + this.state.Baidulat + "," + this.state.Baidulng + "&output=json&pois=1&latest_admin=1&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
+      console.log("url:" + url)
+      fetchJsonp(url, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+          Accept: 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+          // 'Access-Control-Allow-Origin': '*'
+        },
+        jsonpCallbackFunction: 'showLocation'
+      }).then((response) => {
+        return response.json()
+      }).then(data => {
+        let temp = data["result"]
+        temp = temp["formatted_address"]
+        // this.setState({ 
+        //   address: temp
+        // })
+        this.state.address = temp
+        this.setState({
+
+        })
+      })
+      // this.setState({
+      //   Baidulng: temp["x"],
+      //   Baidulat: temp["y"],
+      // })
     })
   }
 
-  BaidutoAddress = () => {
-    let url = "http://api.map.baidu.com/geocoder/v2/?location=" + this.state.Baidulat + "," + this.state.Baidulng + "&output=json&pois=1&latest_admin=1&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
-    console.log("url:"+url)
+
+  // GPStoBaidu = () => {
+  //   let url = "http://api.map.baidu.com/geoconv/v1/?coords=" + this.state.GPSlng + "," + this.state.GPSlat + "&from=1&to=5&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
+  //   console.log("url:"+url)
+  //   fetchJsonp(url, {
+  //     method: "GET",
+  //     mode: 'cors',
+  //     headers: {
+  //       Accept : 'application/json'
+  //       // 'Content-Type': 'application/x-www-form-urlencoded',
+  //       // 'Access-Control-Allow-Origin': '*'
+  //     },
+  //     jsonpCallbackFunction: 'showLocation'
+  //   }).then((response) => {
+  //     return response.json()
+  //   }).then(data => {
+  //     let temp = data["result"]
+  //     temp = temp[0]
+  //     this.state.Baidulng = temp["x"]
+  //     this.state.Baidulat = temp["y"]
+  //     // this.setState({
+  //     //   Baidulng: temp["x"],
+  //     //   Baidulat: temp["y"],
+  //     // })
+  //   })
+  // }
+
+  // BaidutoAddress = () => {
+  //   let url = "http://api.map.baidu.com/geocoder/v2/?location=" + this.state.Baidulat + "," + this.state.Baidulng + "&output=json&pois=1&latest_admin=1&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
+  //   console.log("url:" + url)
+  //   fetchJsonp(url, {
+  //     method: "GET",
+  //     mode: 'cors',
+  //     headers: {
+  //       Accept: 'application/json'
+  //       // 'Content-Type': 'application/x-www-form-urlencoded',
+  //       // 'Access-Control-Allow-Origin': '*'
+  //     },
+  //     jsonpCallbackFunction: 'showLocation'
+  //   }).then((response) => {
+  //     return response.json()
+  //   }).then(data => {
+  //     let temp = data["result"]
+  //     temp = temp["formatted_address"]
+  //     // this.setState({
+  //     //   address: temp
+  //     // })
+  //     this.state.address = temp
+  //   })
+  // }
+
+
+  success = (pos) => {
+    // console.log("GPSlng:",pos.coords.longitude)
+    // console.log("GPSlat:",pos.coords.latitude)
+    this.state.GPSlng = pos.coords.longitude
+    this.state.GPSlat = pos.coords.latitude
+    let url = "http://api.map.baidu.com/geoconv/v1/?coords=" + this.state.GPSlng + "," + this.state.GPSlat + "&from=1&to=5&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
+    console.log("url:" + url)
     fetchJsonp(url, {
       method: "GET",
       mode: 'cors',
       headers: {
-        Accept : 'application/json'
+        Accept: 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
         // 'Access-Control-Allow-Origin': '*'
       },
@@ -60,25 +145,44 @@ export default class App extends Component {
       return response.json()
     }).then(data => {
       let temp = data["result"]
-      temp = temp["formatted_address"]
-      this.setState({
-        address: temp
+      temp = temp[0]
+      this.state.Baidulng = temp["x"]
+      this.state.Baidulat = temp["y"]
+      
+      url = "http://api.map.baidu.com/geocoder/v2/?location=" + this.state.Baidulat + "," + this.state.Baidulng + "&output=json&pois=1&latest_admin=1&ak=mUUbFKtgsaM2rWyuLLIDcyrSkhvGYzmn"
+      console.log("url:" + url)
+      fetchJsonp(url, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+          Accept: 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+          // 'Access-Control-Allow-Origin': '*'
+        },
+        jsonpCallbackFunction: 'showLocation'
+      }).then((response) => {
+        return response.json()
+      }).then(data => {
+        let temp = data["result"]
+        temp = temp["formatted_address"]
+        // this.setState({
+        //   address: temp
+        // })
+        this.state.address = temp
+        this.setState({
+
+        })
       })
-      // this.state.address = temp
-    })
-  }
-
-
-  success = (pos) => {
-    this.setState({
-      GPSlng: pos.coords.longitude,
-      GPSlat: pos.coords.latitude,
+      // this.setState({
+      //   Baidulng: temp["x"],
+      //   Baidulat: temp["y"],
+      // })
     })
   }
 
   error = (err) => {
     this.setState({
-      
+
     })
   }
 
@@ -97,16 +201,25 @@ export default class App extends Component {
 
   render() {
     this.requestLocation()
-    this.GPStoBaidu()
-    this.BaidutoAddress()
-    
+    // if((this.state.GPSlat === '0') && (this.state.GPSlng === '0')){
+    //   this.requestLocation()
+    // }
+    // if((this.state.Baidulat === '0') && (this.state.Baidulng === '0')){
+    //   this.GPStoBaidu()
+    // }
+    // if(this.state.address === '读取中'){
+    //   this.BaidutoAddress()
+    // }
+    // this.GPStoBaidu()
+    // this.BaidutoAddress()
+
 
     return (
       <div style={{ padding: '20px' }}>
         <h1>GPSlng: {this.state.GPSlng} GPSlat: {this.state.GPSlat}</h1>
-        <br/>
+        <br />
         <h1>Baidulng: {this.state.Baidulng} Baidulat: {this.state.Baidulat}</h1>
-        <br/>
+        <br />
         <h1>Address: {this.state.address}</h1>
       </div>
     )
